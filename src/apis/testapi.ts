b1838a7fb4b3e73268a1866db58de6c8d7f12128
LdoDoeg222 <ldodoeg222@outlook.com>
2024-06-14 15:38:03 +0800

    first commit

---
+++ src/apis/testapi.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from "axios"
+import { Res } from "../types/Res"
+
+// import axios from "axios";
+const url = "https://www.searchapi.io/api/v1/search"
+
+const params = {
+  "engine": "google_news",
+  "q": "Jeff Bezos news",
+  "location": "New York,United States",
+  "api_key": "DnTiZCkJrK2HhK19rXUpGepj"
+}
+
+axios({
+  url,
+  params,
+  method: 'GET'
+}).then((resp:AxiosResponse<Res>)=>{
+
+})
+
